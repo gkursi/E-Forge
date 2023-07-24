1ef96902c71bfe2138ea6965ed4d21012df01534
Qweru <119245015+gkursi@users.noreply.github.com>
2023-07-24 15:52:59 +0100

    [+] ability to load more than 1 style'

--- index.js
+++ index.js
@@ -9,39 +9,8 @@ const fs = require('fs');
 const os = require('os');
 const { doesNotReject } = require('assert');
 
-
-
-
-
-
-
-
-function mkdir(dir){
-    if (!fs.existsSync(dir)) {
-        fsp.mkdir(dir)
-        .then(() => console.log("Created directory!"))
-        .catch((err) => {
-          console.error(err);
-          fsError = true;
-        })
-      }
-}
-
-let cssContent = "";
-let fsError = false;
-
-
-
-///////////////////////////////////// CONFIG ///////////////////////////////////////////
-
-let fileToRead = "style.css"
-let fileDirectory = os.homedir()+"/e-forge/"
-
-////////////////////////////////////////////// SETUP /////////////////////////////////////////////////////
-
-mkdir(fileDirectory+fileToRead)
-try {
-    fsp.readFile(fileDirectory+fileToRead, "utf-8")
+/*try {
+    fsp.readFile(fileDirectory+fileDirToRead, "utf-8")
       .then((value) => {
         // console.log(value);
         cssContent = value;
@@ -53,94 +22,149 @@ try {
 } catch (err){
     fsError = true;    
     console.error(err);
+}*/
+
+
+
+
+
+
+function mkdir(dir) {
+  if (!fs.existsSync(dir)) {
+    fsp.mkdir(dir)
+      .then(() => console.log("Created directory!"))
+      .catch((err) => {
+        console.error(err);
+        fsError = true;
+      })
+  }
 }
 
-function applyCSS(){
-  setTimeout(function() {
-        
-    BrowserWindow.getAllWindows()[0].webContents.executeJavaScript(`
+let cssContent = [];
+let fsError = false;
+
+
+
+///////////////////////////////////// CONFIG ///////////////////////////////////////////
+
+let fileDirToRead = "styles/"
+let fileDirectory = os.homedir() + "/e-forge/"
+
+////////////////////////////////////////////// SETUP /////////////////////////////////////////////////////
+
+mkdir(fileDirectory + fileDirToRead)
+fs.readdirSync(fileDirectory + fileDirToRead).forEach(file => {
+  console.log(file);
+  if (file.endsWith(".css")) {
+    let tempdata = "";
+
+    try {
+      fsp.readFile(fileDirectory + fileDirToRead + file, "utf-8")
+        .then((value) => {
+          console.log("File value: " + value);
+          cssContent.push(value);
+        })
+        .catch((err) => {
+          fsError = true;
+          console.error("Got error while reading styles: " + err);
+        })
+    } catch (err) {
+      fsError = true;
+      console.error(err);
+    }
+  }
+});
+
+
+
+function applyCSS() {
+  setTimeout(function () {
+    cssContent.forEach((value, index, array) => {
+      console.log("Applying this CSS: "+value)
+      BrowserWindow.getAllWindows()[0].webContents.executeJavaScript(`
         console.log("Web content created event, applying css...");
 
         docHead = document.querySelector("head")
         docMain = document.querySelector("body")
 
-        docHead.innerHTML = docHead.innerHTML + "<style>`+cssContent.replace(/(\r\n|\n|\r)/gm, "")+`</style>"
+        docHead.innerHTML = docHead.innerHTML + "<style>`+ value.replace(/(\r\n|\n|\r)/gm, "") + `</style>"
     `).then((value) => console.log("Executed CSS!"))
-    .catch((err) => {
-      console.error(err);
-    });
+        .catch((err) => {
+          console.error(err);
+        });
+    })
   }, 1);
 }
 
 //////////////////////////////////// WINDOWS /////////////////////////////////////////
 
 const createWindow = () => {
-  
-    if(fsError){
-        const win = new BrowserWindow({
-            width: 700,
-            height: 200,
-            resizable: false,
-            fullscreenable: false,
-            frame: true,
-            darkTheme: true,
-            movable: false,
-            title: "Main load error"
-          //   webPreferences: {
-          //     preload: path.join(__dirname, './html/js/preload.js')
-          //   }
-          })
-        
-          // win.loadFile('html/index.html')
-          win.loadFile("html/css_not_found.html")
 
-          win.webContents.on("dom-ready", () => {
-            console.log("DOM event triggered");
-            applyCSS();
-          })
+  if (fsError) {
+    const win = new BrowserWindow({
+      width: 700,
+      height: 200,
+      resizable: false,
+      fullscreenable: false,
+      frame: true,
+      darkTheme: true,
+      movable: false,
+      title: "Main load error"
+      //   webPreferences: {
+      //     preload: path.join(__dirname, './html/js/preload.js')
+      //   }
+    })
 
-    } else {
-        const win = new BrowserWindow({
-            width: 800,
-            height: 600,
-          //   resizable: false,
-            fullscreenable: false,
-            frame: true,
-            darkTheme: true,
-            movable: true,
-            title: "Loading..",
-          //   webPreferences: {
-          //     preload: path.join(__dirname, './html/js/preload.js')
-          //   }
-          })
-        
-          // win.loadFile('html/index.html')
-          win.loadURL("https://e-klase.lv");
-          
-          win.webContents.on("dom-ready", () => {
-            console.log("DOM event triggered");
-            applyCSS();
-          })
+    // win.loadFile('html/index.html')
+    win.loadFile("html/css_not_found.html")
 
-    }
+    win.webContents.on("dom-ready", () => {
+      console.log("DOM event triggered");
+      applyCSS();
+    })
+
+  } else {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      //   resizable: false,
+      fullscreenable: false,
+      frame: true,
+      darkTheme: true,
+      movable: true,
+      title: "Loading..",
+      //   webPreferences: {
+      //     preload: path.join(__dirname, './html/js/preload.js')
+      //   }
+    })
+
+    // win.loadFile('html/index.html')
+    win.loadURL("https://e-klase.lv");
+
+    win.webContents.on("dom-ready", () => {
+      console.log("DOM event triggered");
+      applyCSS();
+    })
+
+  }
 }
 
 /////////////////////////////////////////// EVENTS /////////////////////////////////////////////////
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow()
 
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+})
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+  if (process.platform !== 'darwin') app.quit()
+})
 
 app.on('web-contents-created', (webContents) => {
-    
-    console.log("Web content created! (event)")
-    applyCSS();
+
+  console.log("Web content created! (event)")
+  applyCSS();
 })
 
